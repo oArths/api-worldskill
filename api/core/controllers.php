00a6e33e5e13b13653286ba43781c2ceb2d498a7
Arthur <arthurferreira1417@gmail.com>
2024-05-31 22:33:56 -0300

    Feat: criando o endpoint de criar usuario

--- api/core/controllers.php
+++ api/core/controllers.php
@@ -1,5 +1,6 @@
 <?php
 include_once(dirname(__FILE__) . '/../config/database.php');
+include_once(dirname(__FILE__) . '/../core/auth.php');
 
 
 class Controllers{
@@ -34,6 +35,7 @@ class Controllers{
     }
     public function create_new_user ($params){
         $db = new database;
+        $authentic = new Auth;
 
         if(
             !isset($params['email']) ||
@@ -41,8 +43,11 @@ class Controllers{
         !isset($params['name']) ||
         !isset($params['username'])    
         ){
-            return $this->erro_response('campos insuficientes');
-            // return $params;
+            return $this->erro_response('campos insuficientes1');
+        }
+        if( strlen($params['password']) < 6 ){
+            return $this->erro_response("Invalid properties2");
+
         }
         $consulta = [
             ':email' => $params['email'],
@@ -52,32 +57,61 @@ class Controllers{
         $results = $db->QUERY('SELECT id FROM user WHERE email = :email OR  username =:username', $consulta);
         if(count($results) != 0  ){
             http_response_code(422);
-            return $this->erro_response("Invalid properties");
+            return $this->erro_response("Invalid properties3");
         }
-        date_default_timezone_set('America/Sao_Paulo');
-        $pass = password_hash($params['password'], PASSWORD_DEFAULT);
-        $randomHash = password_hash(random_bytes(32), PASSWORD_DEFAULT);
-
-        $token = $randomHash . date(" Y-m-d H:i:s");
         
-
-        $params = [
+        
+        $pass = password_hash($params['password'], PASSWORD_DEFAULT);
+        
+        $Insert = [
             ':name' => $params['name'],
             ':email' => $params['email'],
             ':username' => $params['username'],
             ':password' => $pass,
         ];
 
+
         $db-> QUERY("
         INSERT INTO user (name, email, username, password) VALUES
-        (:name,:email,:username,:password)", $params);
+        (:name,:email,:username,:password)", $Insert);
 
+         $token = $authentic->create_token($params);
         return [
             'token' => $token,
         ];
     }
-  
-}
+    public function login_user ($params) {
+        $db = new database;
+        $auth = new Auth;
+
+        // return $params;
+        // a validação ta errada pq 
+        if((!isset($params['email']) && !isset($params['password'])) && (strlen($params['password']) < 6)){
+            http_response_code(422);
+            return $this->erro_response("Invalid properties4");      
+        }
+
+        $pass = password_hash($params['password'], PASSWORD_DEFAULT);
+
+        $consulta = [
+            ':email' => $params['email'],
+            ':password' => $pass
+
+        ];
+        
+        $results = $db->QUERY('SELECT * FROM user WHERE email = :email AND password = :password', $consulta);
+        return $results;
+        if(empty($results)){
+            http_response_code(422);
+            return $this->erro_response("Invalid properties5");
+        }
+
+        $exist = $auth->exist_token($params);
+        
+        if($exist){
+
+        }
+    }}
 
 
 ?>
