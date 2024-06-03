fc269309056eebe755f934dadd04df0e516e4b6d
Arthur <arthurferreira1417@gmail.com>
2024-06-02 22:18:35 -0300

    Feat: arrumando as resposta do header

--- index.php
+++ index.php
@@ -20,8 +20,7 @@ $router->add('POST', $url . '/api/v1/auth/signup', function() use ($control) {
 
      $response = $control->create_new_user($data);
 
-     header('Content-Type: application/json');
-     http_response_code(201);
+   
      echo json_encode($response);
     
     });
@@ -31,8 +30,18 @@ $router->add('POST', $url . '/api/v1/signin', function() use ($control) {
 
      $response = $control->login_user($data);
 
+     
+     echo json_encode($response);
+    
+    });
+$router->add('DELETE', $url . 'api/v1/auth/signout', function() use ($control) {
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+     $response = $control->token_delete($data);
+
      header('Content-Type: application/json');
-     http_response_code(201);
+     http_response_code(204);
      echo json_encode($response);
     
     });
