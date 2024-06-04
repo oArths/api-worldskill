96a34dcfb2619c0cf0d1e35c153333d94a74489f
unknown <arthurferreira1417@gmail.com>
2024-06-03 21:51:58 -0300

    Feat: ajustando a função pra separar o caminho base do restos dos paramewtros

--- api/core/router.php
+++ api/core/router.php
@@ -14,17 +14,35 @@ class Router {
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['REQUEST_URI'];
 
+
+
+    if(strpos($path, '?') !== false){
+      $clear = explode("?", $path);
+      $newpath = $clear[0];
+
+
+      foreach ($this->routes as $route) {
+        if ($route['method'] === $method && $route['path'] === $newpath) {
+            call_user_func($route['callback']);
+            return ;
+        }
+    }
+
+    http_response_code(404);
+    echo json_encode(['message' => 'Endpoint not found',]);
+
+    }else{
         foreach ($this->routes as $route) {
             if ($route['method'] === $method && $route['path'] === $path) {
                 call_user_func($route['callback']);
-                return;
+                return ;
             }
         }
 
         http_response_code(404);
         echo json_encode(['message' => 'Endpoint not found',]);
-        // echo json_encode(['message' => $method,]);
-        // echo json_encode(['message' => $path]);
+    
     }
-}
-?>
+       
+}}
+
