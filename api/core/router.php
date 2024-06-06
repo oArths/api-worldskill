65a9ff40a987e9721c82d6c8bb3aa7ddbac8f24e
unknown <arthurferreira1417@gmail.com>
2024-06-05 22:15:37 -0300

    Fix: remodelei em como as rotas são verificadas e a corrigi os erros

--- api/core/router.php
+++ api/core/router.php
@@ -3,85 +3,66 @@ class Router {
     private $routes = [];
 
     public function add($method, $path, $callback) {
-        $pattern = preg_replace('/\//', '\\/', $path);
-        $pattern = '/^' . $pattern . '$/';
         $this->routes[] = [
             'method' => $method,
-            'pattern' => $pattern,
+            'path' => $path,
             'callback' => $callback
         ];
     }
-
+         
+        
     public function run() {
         $method = $_SERVER['REQUEST_METHOD'];
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $path = $_SERVER['REQUEST_URI'];
 
-        foreach ($this->routes as $route) {
-            if ($route['method'] === $method && preg_match($route['pattern'], $path, $matches)) {
-                array_shift($matches);
-                call_user_func_array($route['callback'], $matches);
-                return;
+        $id = explode('/', $path);
+        $end = end($id);
+
+
+        if(preg_match('/^[0-9]+$/', $end)){
+           array_pop($id);
+           $newpath = implode('/', $id) ;
+            $newpath = $newpath . '/';
+           foreach ($this->routes as $route) {
+            if ($route['method'] === $method && $route['path'] === $newpath) {
+                header('Content-Type: application/json');
+                call_user_func($route['callback']);
+                return ;
             }
-        }
-
+        }   
         http_response_code(404);
-        echo json_encode(['message' => 'Endpoint not found']);
-    }
+        header('Content-Type: application/json');
+        echo json_encode(['message' => 'Endpoint not found1']);
+        
+        }elseif($_SERVER['QUERY_STRING']){
+            $clear = explode("?", $path);
+            $newpath = $clear[0];
+            
+            foreach ($this->routes as $route) {
+                if ($route['method'] === $method && $route['path'] === $newpath) {
+                    header('Content-Type: application/json');
+                    call_user_func($route['callback']);
+                    return ;
+                }
+                
+            }
+            http_response_code(404);
+            header('Content-Type: application/json');
+            echo json_encode(['message' => 'Endpoint not found2']);
+        }else{
+
+            foreach ($this->routes as $route) {
+                if ($route['method'] === $method && $route['path'] === $path) {
+                    header('Content-Type: application/json');
+                    call_user_func($route['callback']);
+                    return ;
+                }
+                
+            }
+            http_response_code(404);
+            header('Content-Type: application/json');
+            echo json_encode(['message' => 'Endpoint not found3',]);
+        }
+    }    
+    
 }
-
-    // public function rota (){
-    //     $method = $_SERVER['REQUEST_METHOD'];
-    //     $path = $_SERVER['REQUEST_URI'];
-
-    //     foreach ($this->routes as $route) {
-    //                 if ($route['method'] === $method && $route['path'] === $path) {
-    //                     call_user_func($route['callback']);
-    //                     return ;
-    //                 }
-    //             }
-    // }
-
-    // public function run() {
-    //     $method = $_SERVER['REQUEST_METHOD'];
-    //     $path = $_SERVER['REQUEST_URI'];
-    //     $last_data = strrpos($path, '/');
-
-    // $rota = $this->rota();
-
-    // if($rota === false){
-    //     http_response_code(404);
-    //      echo json_encode(['message' => 'Endpoint not found1',]);
-         
-    // }elseif(strpos($path, '?') !== false){
-    //     $clear = explode("?", $path);
-    //       $newpath = $clear[0];
-    
-    
-    //       foreach ($this->routes as $route) {
-    //         if ($route['method'] === $method && $route['path'] === $newpath) {
-    //             call_user_func($route['callback']);
-    //             return ;
-    //         }
-    //     }
-    
-    //     http_response_code(404);
-    //     echo json_encode(['message' => 'Endpoint not found2',]);
-       
-    // }else if($last_data !== false){
-    //         $path = substr($path, 0, $last_data + 1);
-          
-    //         foreach ($this->routes as $route) {
-    //             if ($route['method'] === $method && $route['path'] === $path) {
-    //                 call_user_func($route['callback']);
-    //                 return ;
-    //             }
-    //     }   
-    // }
-         
-    //      http_response_code(404);
-    //      echo json_encode(['message' => 'Endpoint not found3',]);
-
-       
-    // }
-
-
