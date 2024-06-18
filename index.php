67c3cacd1946bc91ff56ee8bfae0784518c4afe6
unknown <arthurferreira1417@gmail.com>
2024-06-18 15:30:45 -0300

    Feat: finalizando a rota de deletar review do filme pelo id

--- index.php
+++ index.php
@@ -247,6 +247,31 @@ $router->add('DELETE', $url . '/api/v1/reviews/evaluations', function() use ($co
     echo json_encode($response);
 
 });
+$router->add('DELETE', $url . '/api/v1/reviews', function() use ($control){
+    $id = $_GET;
+    $header = getallheaders();
+   
+
+    if(!isset($header['Authorization']) || empty($header['Authorization'])){
+        http_response_code(401);
+        echo json_encode($control->erro_messenge('Unauthenticated user'));
+        return ;
+    }else{
+        $token = explode(" ", $header['Authorization'])[1];
+    }
+
+    $data = [
+        'data'=> $id,
+        'token' => $token    
+    ];
+
+    $response = $control->delete_reviews_movie($data);
+
+    echo json_encode($response);
+
+
+
+});
 
 $router->run();
 
